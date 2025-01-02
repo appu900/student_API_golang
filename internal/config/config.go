40cb87c0def23a13237565b4dd83da8210f3a916
pabitra <89703515+appu900@users.noreply.github.com>
2025-01-02 19:14:39 +0530

    parse config file

---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HttpServer struct {
+	Address string
+}
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	HttpServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "path to configuration file")
+		flag.Parse()
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is required")
+		}
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("config file not found")
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("config: %v", err.Error())
+	}
+
+	return &cfg
+}
